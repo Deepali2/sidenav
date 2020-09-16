b2a54b2bd8067d3acf348407ec9d37ab417902a9
Deepali Garg <d.garg@partner.samsung.com>
2020-09-16 09:52:26 -0700

    use ids instead of innerHTML

--- src/NewAudience/SideNav.js
+++ src/NewAudience/SideNav.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Dropdown, DropdownButton } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -7,12 +7,12 @@ const SideNav = ({ segments, segment, setSegment, country, setCountry }) => {
 
   const changeCountry = (e) => {
     if (e.target.tagName.toLowerCase() === 'a') {
-      setCountry(e.target.innerHTML);
+      setCountry(e.target.id);
     }
   }
 
   const changeSegment = (e) => {
-    setSegment(e.target.innerHTML);
+    setSegment(e.target.id);
   }
   return (
     <>
@@ -20,13 +20,13 @@ const SideNav = ({ segments, segment, setSegment, country, setCountry }) => {
         <label htmlFor="country-dropdown">Audience Country</label>
         <DropdownButton id="country-dropdown" title={country} onClick={changeCountry}>
           {countries.map((country, index) => (
-            <Dropdown.Item href="#" key={index}>{country}</Dropdown.Item>
+            <Dropdown.Item id={country} key={index}>{country}</Dropdown.Item>
           ))}        
         </DropdownButton>
       </div>
       <ul>
         {segments.map((element, index) => (
-          <li key={index} onClick={changeSegment}>{element}</li>
+          <li key={index} id={element} onClick={changeSegment}>{element}</li>
         ))}
       </ul>      
     </>
