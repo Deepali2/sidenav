e9b7a525b6336f797993bd8ae9d59717295fb0c3
Deepali Garg <d.garg@partner.samsung.com>
2020-09-15 22:22:23 -0700

    use div instead of p tag

--- src/NewAudience/SideNav.js
+++ src/NewAudience/SideNav.js
@@ -1,19 +1,13 @@
 import React from "react";
-// import { Nav } from "react-bootstrap";
-import { Table } from "react-bootstrap";
 
 import { segments } from './data';
 
 const SideNav = props => {
   return (
-      <>
-        <Table>
-         {segments.map((segment, index) => (
-           <tr>
-             <p key={index}>{segment}</p>
-           </tr>
-         ))}
-        </Table>
+      <>        
+        {segments.map((segment, index) => (           
+          <div key={index}>{segment}</div>           
+        ))}
       </>
   );
 };
