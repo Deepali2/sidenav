43b6e6a6978a3d60d7c4640eee71d05905b1ca32
Deepali Garg <d.garg@partner.samsung.com>
2020-09-16 00:14:35 -0700

    add the country checkbox

--- src/NewAudience/SideNav.js
+++ src/NewAudience/SideNav.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 const SideNav = ({ segments, segment, setSegment }) => {
+  const countries = ['UnitedStates', 'Canada', 'Spain', 'India']
   const changeSegment = (e) => {
     setSegment(e.target.innerHTML);
   }
   return (
     <>
-      <div>Countries Selection</div>
+      <div>
+        <label htmlFor="country-dropdown">Audience Country</label>
+        <DropdownButton id="country-dropdown" title="Select">
+          {countries.map((country, index) => (
+            <Dropdown.Item href="#" key={index}>{country}</Dropdown.Item>
+          ))}        
+        </DropdownButton>
+      </div>
       <ul>
         {segments.map((element, index) => (
           <li key={index} onClick={changeSegment}>{element}</li>
